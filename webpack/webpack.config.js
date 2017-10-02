c5731d0dd3a761e11cd484edfdcbfcccb324672c
strongcode9527 <1477869317@qq.com>
2017-10-02 14:18:25 +0800

    使用react-dev-utils构建应用

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,20 +1,21 @@
-var webpack = require('webpack')
-var merge = require('webpack-merge')
-var baseConfig = require('./webpack.base.js')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const webpack = require('webpack'),
+  merge = require('webpack-merge'),
+  baseConfig = require('./webpack.base.js'),  
+  HtmlWebpackPlugin = require('html-webpack-plugin')
 
+  
 var config = merge(
   baseConfig, {
     plugins: [
       new HtmlWebpackPlugin({
         template: 'index.html'
       }),
-      new OpenBrowserPlugin({ url: 'http://localhost:3001' }),
       new webpack.HotModuleReplacementPlugin()
     ],
     devtool: 'inline-source-map',
   }
 )
+
+
 
 module.exports = config
